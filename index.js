c50beaea65636e051505c50690ecac8b15acb916
= <=>
2020-06-21 16:09:02 -0700

    Interval fix

--- index.js
+++ index.js
@@ -298,10 +298,10 @@ function startWorkTimer() {
     document.getElementById("status").style = "color: rgb(255, 132, 110);";
     document.getElementById("status").innerText = "WORKING...";
     document.getElementById("time").innerText = 25;
-    setWorkCountdown = setInterval(workCountdown, 1000);
+    setWorkCountdown = setInterval(workCountdown, 60000);
 }
 function startBreakTimer() {
-    setBreakCountdown = setInterval(breakCountdown, 1000);
+    setBreakCountdown = setInterval(breakCountdown, 60000);
 }
 function breakCountdown() {
     while(breakInterval > 0) {
